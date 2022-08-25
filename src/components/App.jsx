98fa2b151f156d98775d747c92e3c211412e9222
Nedzhat <nexuak@gmail.com>
2022-08-25 10:19:23 +0300

    fix methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,28 +9,24 @@ export class App extends Component {
   state = {
   good: 0,
   neutral: 0,
-  bad: 0
+  bad: 0,
   }
   
-  addedFeedback = evt => {
-    const curBtn = evt.target.textContent.toLowerCase();
-    for (const key in this.state) {
-      if (key === curBtn) {
-           this.setState((prevState) => {
+  addFeedback = feedback => {
+    let feedbackNormalized = feedback.toLowerCase();
+    this.setState((prevState) => {
       return {
-        [key]: prevState[key] + 1,
+        [feedbackNormalized]: prevState[feedbackNormalized] + 1,
       }
     })
-      }
-    }
-  } 
+  }
 
   countTotalFeedback = () => {
-    let total = 0;
-    for (const key in this.state) {
-      total += this.state[key];
-      }
-    return total;
+    let total = Object.values(this.state);
+    const totalFeedback = total.reduce((prev, number) => {
+      return prev + number;
+    }, 0);
+    return totalFeedback;
   }
 
   countPositiveFeedbackPercentage = (total) => {
@@ -48,7 +44,7 @@ export class App extends Component {
     <div>
         <GlobalStyle />
         <Section title='Please leave feedback'>
-          <FeedbackOptions options={this.state} onLeaveFeedback={this.addedFeedback}/>
+          <FeedbackOptions options={this.state} onLeaveFeedback={this.addFeedback}/>
           {this.countTotalFeedback() > 0 ? (<Statistics good={good} neutral={neutral} bad={bad} onTotal={this.countTotalFeedback()} onCheckPercentage={this.countPositiveFeedbackPercentage(this.countTotalFeedback())}/>) : <Notification message="There is no feedback" />}
         </Section >
       </div>
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -7,7 +7,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     const arrBtn = Object.keys(options);
     return <Box p={3}
     display="flex" justifyContent="space-evenly" >{arrBtn.map((value, idx) => {
-        return <Btn onClick={onLeaveFeedback} key={idx}>{value}</Btn>
+        return <Btn onClick={() => onLeaveFeedback(value)} key={idx}>{value}</Btn>
     })}
     </Box>
 }
